a34d88e80ea8a010a460ca01fb45ed8c3e405361
marty-suzuki <s1180183@gmail.com>
2017-08-06 21:35:48 +0900

    add frameworks

---
+++ Carthage/Build/iOS/GithubKit.framework/Headers/GithubKit.h
@@ -0,0 +1,19 @@
+//
+//  GithubKit.h
+//  GithubKit
+//
+//  Created by marty-suzuki on 2017/08/05.
+//  Copyright © 2017年 marty-suzuki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GithubKit.
+FOUNDATION_EXPORT double GithubKitVersionNumber;
+
+//! Project version string for GithubKit.
+FOUNDATION_EXPORT const unsigned char GithubKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GithubKit/PublicHeader.h>
+
+
---
+++ Carthage/Build/iOS/SwiftIconFont.framework/Headers/SwiftIconFont.h
@@ -0,0 +1,19 @@
+//
+//  SwiftIconFont.h
+//  SwiftIconFont
+//
+//  Created by Sedat Gökbek ÇİFTÇİ on 12/07/16.
+//  Copyright © 2016 Sedat Gökbek ÇİFTÇİ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftIconFont.
+FOUNDATION_EXPORT double SwiftIconFontVersionNumber;
+
+//! Project version string for SwiftIconFont.
+FOUNDATION_EXPORT const unsigned char SwiftIconFontVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftIconFont/PublicHeader.h>
+
+
