84a4bd2e1f5c68bc6380f06516df80071ae31cc6
marty-suzuki <s1180183@gmail.com>
2017-09-11 23:51:15 +0900

    update framework

---
+++ Carthage/Build/iOS/FluxCapacitor.framework/Headers/FluxCapacitor.h
@@ -0,0 +1,19 @@
+//
+//  FluxCapacitor.h
+//  FluxCapacitor
+//
+//  Created by marty-suzuki on 2017/08/07.
+//  Copyright © 2017年 marty-suzuki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FluxCapacitor.
+FOUNDATION_EXPORT double FluxCapacitorVersionNumber;
+
+//! Project version string for FluxCapacitor.
+FOUNDATION_EXPORT const unsigned char FluxCapacitorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FluxCapacitor/PublicHeader.h>
+
+
