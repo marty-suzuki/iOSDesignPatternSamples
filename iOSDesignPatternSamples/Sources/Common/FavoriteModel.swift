82f41047c541cdab7c102934a35841613f0b8fd9
marty-suzuki <s1180183@gmail.com>
2021-02-13 00:23:30 +0900

    Fix FavoriteModel

--- iOSDesignPatternSamples/Sources/Common/FavoriteModel.swift
+++ iOSDesignPatternSamples/Sources/Common/FavoriteModel.swift
@@ -11,69 +11,51 @@ import GithubKit
 
 protocol FavoriteModelType: AnyObject {
     var favorites: [Repository] { get }
-    var contains: AnyPublisher<(Bool, Repository), Never> { get }
+    var favoritePublisher: Published<[Repository]>.Publisher { get }
     func addFavorite(_ repository: Repository)
     func removeFavorite(_ repository: Repository)
+    func contains(_ repository: Repository) -> AnyPublisher<Bool, Never>
 }
 
 final class FavoriteModel: FavoriteModelType {
-
-    let contains: AnyPublisher<(Bool, Repository), Never>
+    @Published
+    private(set) var favorites: [Repository] = []
+    var favoritePublisher: Published<[Repository]>.Publisher {
+        $favorites
+    }
 
     private let _addFavorite = PassthroughSubject<Repository, Never>()
     private let _removeFavorite = PassthroughSubject<Repository, Never>()
-
-    @Published
-    private(set) var favorites: [Repository] = []
     private var cancellables = Set<AnyCancellable>()
 
-    init(
-        repository: Repository,
-        favoritesInput: @escaping ([Repository]) -> Void,
-        favoritesOutput: AnyPublisher<[Repository], Never>
-    ) {
-
-        self.contains = favoritesOutput
-            .map { favorites in
-                (favorites.contains { $0.url == repository.url }, repository)
+    init() {
+        let favorites1 = _addFavorite
+            .flatMap { [weak self] repository -> AnyPublisher<[Repository], Never> in
+                guard let me = self else {
+                    return Empty().eraseToAnyPublisher()
+                }
+                var favorites = me.favorites
+                if favorites.firstIndex(where: { $0.url == repository.url }) != nil {
+                    return Empty().eraseToAnyPublisher()
+                }
+                favorites.append(repository)
+                return Just(favorites).eraseToAnyPublisher()
             }
-            .eraseToAnyPublisher()
 
-        do {
-            let favorites1 = _addFavorite
-                .flatMap { [weak self] repository -> AnyPublisher<[Repository], Never> in
-                    guard let me = self else {
-                        return Empty().eraseToAnyPublisher()
-                    }
-                    var favorites = me.favorites
-                    if favorites.firstIndex(where: { $0.url == repository.url }) != nil {
-                        return Empty().eraseToAnyPublisher()
-                    }
-                    favorites.append(repository)
-                    return Just(favorites).eraseToAnyPublisher()
+        let favorites2 = _removeFavorite
+            .flatMap { [weak self] repository -> AnyPublisher<[Repository], Never> in
+                guard let me = self else {
+                    return Empty().eraseToAnyPublisher()
                 }
-
-            let favorites2 = _removeFavorite
-                .flatMap { [weak self] repository -> AnyPublisher<[Repository], Never> in
-                    guard let me = self else {
-                        return Empty().eraseToAnyPublisher()
-                    }
-                    var favorites = me.favorites
-                    guard let index = favorites.firstIndex(where: { $0.url == repository.url }) else {
-                        return Empty().eraseToAnyPublisher()
-                    }
-                    favorites.remove(at: index)
-                    return Just(favorites).eraseToAnyPublisher()
+                var favorites = me.favorites
+                guard let index = favorites.firstIndex(where: { $0.url == repository.url }) else {
+                    return Empty().eraseToAnyPublisher()
                 }
+                favorites.remove(at: index)
+                return Just(favorites).eraseToAnyPublisher()
+            }
 
-            favorites1.merge(with: favorites2)
-                .sink { favorites in
-                    favoritesInput(favorites)
-                }
-                .store(in: &cancellables)
-        }
-
-        favoritesOutput
+        favorites1.merge(with: favorites2)
             .assign(to: \.favorites, on: self)
             .store(in: &cancellables)
     }
@@ -84,5 +66,11 @@ final class FavoriteModel: FavoriteModelType {
 
     func removeFavorite(_ repository: Repository) {
         _removeFavorite.send(repository)
+    }
+
+    func contains(_ repository: Repository) -> AnyPublisher<Bool, Never> {
+        $favorites
+            .map { $0.contains { $0.url == repository.url } }
+            .eraseToAnyPublisher()
     }
 }
